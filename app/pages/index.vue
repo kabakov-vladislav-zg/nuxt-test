905834a3b5685a7a6d0867303064f50a9e938055
vladislav <kabakov.vladislav@adinadin.ru>
2025-11-20 00:08:05 +0400

    feat: add shared pagination ui and composable

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { usePostsStore } from '~/entities/posts';
+import { usePagination } from '~/shared/model/usePagination';
+import AppPagination from '~/shared/ui/AppPagination.vue';
+
+const store = usePostsStore();
+callOnce(store.fetchPosts);
+const itemsPerPage = 8;
+const { page, currentList } = usePagination(() => store.postsList, itemsPerPage);
+</script>
+
 <template>
   <UContainer class="mt-10">
-    test
+    <AppPagination
+      v-model:page="page"
+      :items-per-page="itemsPerPage"
+      :total="store.postsList.length"
+    />
+    <pre>
+      {{ currentList }}
+    </pre>
   </UContainer>
 </template>
---
+++ app/shared/model/usePagination.ts
@@ -0,0 +1,8 @@
+import { chunk } from 'lodash';
+
+export function usePagination<T>(list: MaybeRefOrGetter<T[]>, itemsPerPage: MaybeRefOrGetter<number>) {
+  const page = ref(1);
+  const listChunk = computed(() => chunk(toValue(list), toValue(itemsPerPage)));
+  const currentList = computed(() => listChunk.value[page.value - 1]!);
+  return { page, currentList }
+}
