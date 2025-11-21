3b7b70f9211c07f097e6e723d485067257bc2884
vladislav <kabakov.vladislav@adinadin.ru>
2025-11-21 16:52:33 +0400

    fix: add shared pagination ui

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,17 +1,27 @@
 export default defineAppConfig({
   ui: {
-    pagination: {
-      slots: {
-        root: '',
-        list: '',
-        ellipsis: '',
-        label: '',
-        first: 'hidden',
-        prev: 'hidden',
-        item: '',
-        next: '',
-        last: 'hidden'
+    blogPosts: {
+      base: '',
+      variants: {
+        orientation: {
+          horizontal: 'sm:grid sm:grid-cols-2 lg:grid-cols-4',
+          vertical: ''
+        }
       }
-    }
-  }
+    },
+    button: {
+      compoundVariants: [
+        {
+          color: 'neutral',
+          variant: 'soft',
+          class: 'rounded-xl! bg-[#F3F3F3]! hover:bg-[#E8E8E8]! active:bg-[#E8E8E8]! focus-visible:bg-[#E8E8E8]!'
+        },
+        {
+          color: 'neutral',
+          variant: 'outline',
+          class: 'ring-[#E8E8E8]! rounded-xl! border-[#E8E8E8]! hover:bg-[#E8E8E8]! active:bg-[#E8E8E8]! focus-visible:bg-[#E8E8E8]!'
+        },
+      ],
+    },
+  },
 })
---
+++ app/shared/ui/AppPagination.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui';
+
+defineProps<{
+  to?: (page: number) => ButtonProps['to'],
+  itemsPerPage?: number,
+  total?: number,
+}>()
+
+const page = defineModel<number>('page')
+</script>
+
+<template>
+  <UPagination
+    v-model:page="page"
+    :items-per-page="itemsPerPage"
+    :total="total"
+    :ui="{
+        list: 'gap-2!',
+        first: 'hidden',
+        prev: 'hidden',
+        last: 'hidden',
+        next: 'p-2.5 *:size-6',
+    }"
+  >
+    <template #item="{ item }">
+      <UButton
+        v-if="item.type === 'page'"
+        :color="page === item.value! ? 'neutral' : 'neutral'"
+        :variant="page === item.value! ? 'solid' : 'soft'"
+        :label="String(item.value)"
+        :to="to?.(item.value)"
+        class="size-11 flex justify-center items-center text-base"
+        square 
+      />
+    </template>
+  </UPagination>
+</template>
