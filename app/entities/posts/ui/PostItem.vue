2693f3901c27c9c583d2b94b152d67cdd6072c00
vladislav <kabakov.vladislav@adinadin.ru>
2025-11-23 13:06:30 +0400

    fix: post item hover state

--- app/entities/posts/ui/PostItem.vue
+++ app/entities/posts/ui/PostItem.vue
@@ -10,31 +10,32 @@ defineProps<{
 </script>
 
 <template>
-  <UBlogPost
-    :description="post.preview"
-    :image="post.image"
-    :to="to"
-    variant="naked"
-    :ui="{
-      root: 'rounded-none pt-6! overflow-hidden!',
-      header: 'aspect-square rounded-none shadow-none transform transition-transform duration-200 group-hover/blog-post:-translate-y-6!',
-      body: 'px-0! pb-3! transform transition-transform duration-200 group-hover/blog-post:-translate-y-6!',
-      footer: 'absolute top-full start-0 end-0 transform transition-transform duration-200 group-hover/blog-post:-translate-y-6!',
-      description: 'text-xl/[1.2] text-text'
-    }"
-  >
-    <template #header>
-      <NuxtImg
-        class="object-cover object-top w-full h-full"
-        :src="post.image"
-        :placeholder="logo"
-        placeholder-class="object-contain! object-center! scale-50"
-      />
-    </template>
-    <template #footer>
-      <div class="text-secondary text-xl/[1.2]">
-        Read more
-      </div>
-    </template>
-  </UBlogPost>
+  <div class="flex flex-col flex-1 overflow-hidden! *:transform *:transition-transform *:duration-200 hover:*:-translate-y-6!">
+    <UBlogPost
+      :description="post.preview"
+      :image="post.image"
+      :to="to"
+      variant="naked"
+      :ui="{
+        root: 'flex-1 -mb-6! rounded-none pt-6!',
+        header: 'aspect-square rounded-none shadow-none',
+        body: 'px-0! pb-3!',
+        description: 'text-xl/[1.2] text-text'
+      }"
+    >
+      <template #header>
+        <NuxtImg
+          class="object-cover object-top w-full h-full"
+          :src="post.image"
+          :placeholder="logo"
+          placeholder-class="object-contain! object-center! scale-50"
+        />
+      </template>
+      <template #footer>
+        <div class="text-secondary text-xl/[1.2]">
+          Read more
+        </div>
+      </template>
+    </UBlogPost>
+  </div>
 </template>
